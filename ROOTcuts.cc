b1b8a7495ba92393c60a061f65c2b1ee4f0ac8ff
Alexander Titterton <alextitterton@physics.org>
2016-06-01 01:19:53 +0100

    -ve offset

--- ROOTcuts.cc
+++ ROOTcuts.cc
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
         histMbbBkg->SetLineColor(kRed);
         
         hs->Draw("nostack");
-		hs->GetYaxis()->SetTitleOffset(0.05);
+		hs->GetYaxis()->SetTitleOffset(-0.1);
         cmbb->Update();
         
         TLegend *legend = new TLegend(0.1, 0.7, 0.48, 0.9);
