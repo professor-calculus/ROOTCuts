b55b7c46509e14ed1db87d07c5b99a239706ddef
professor-calculus <alextitterton@physics.org>
2016-11-02 13:51:35 +0000

    Edited auto-install.sh to fix problems with certain ROOT versions & gcc versions, now works on Bristol cluster with CMSSW 8_0_19 bundled versions of ROOT & gcc

--- bin/auto-install.sh
+++ bin/auto-install.sh
@@ -2,6 +2,7 @@ echo 'Installing latest version of Delphes'
 cd ..
 git clone https://github.com/delphes/delphes.git
 cd delphes
+sed -i '/save/d; /loadFromFile/d' display/Delphes3DGeometry.h
 make
 cd ../bin
 echo 'Delphes installed, now installing ROOTCuts :)'
